4bcc7b3f5820e02ddb033607367a1423ad9e9630
Jeremiah Sanders <1328897+JeremiahSanders@users.noreply.github.com>
2022-03-27 18:41:17 -0500

    task: add generated markdown api docs

--- ci/bin/ci-workflows.sh
+++ ci/bin/ci-workflows.sh
@@ -62,8 +62,28 @@ ci-validate() {
 # Compose the project's artifacts, e.g., compiled binaries, Docker images.
 #--
 ci-compose() {
-  ci-dotnet-publish &&
-    ci-dotnet-pack
+  function createDocs() {
+    local sourcePath="${BUILD_UNPACKAGED_DIST}/LanguageExt.Extras.dll"
+    local outputPath="${BUILD_DOCS}/md"
+    xmldocmd "${sourcePath}" "${outputPath}" \
+      --namespace "Jds.LanguageExt.Extras" \
+      --source "https://github.com/JeremiahSanders/languageext-extras/tree/main/src" \
+      --newline lf \
+      --visibility public
+  }
+  ci-dotnet-publish \
+    -p:GenerateDocumentationFile=true &&
+    ci-dotnet-pack \
+      -p:GenerateDocumentationFile=true &&
+    createDocs
+}
+
+regenerate-docs() {
+  if [[ ! -d "${PROJECT_ROOT}/docs/api" ]]; then
+    mkdir "${PROJECT_ROOT}/docs/api"
+  fi
+  ci-compose &&
+    cp -R "${BUILD_DOCS}/md/." "${PROJECT_ROOT}/docs/api/"
 }
 
 #--

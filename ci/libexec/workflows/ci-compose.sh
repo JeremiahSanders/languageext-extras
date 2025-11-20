4d81a20a1a2c05e6ce348235bf50f129a32eda88
Jeremiah Sanders <1328897+JeremiahSanders@users.noreply.github.com>
2025-11-19 19:51:42 -0600

    feat: add .net 8 and 10 support

--- ci/libexec/workflows/ci-compose.sh
+++ ci/libexec/workflows/ci-compose.sh
@@ -18,20 +18,45 @@ set -o pipefail # Fail pipelines if any command errors, not just the last one.
 
 function ci-compose() {
   printf "Composing build artifacts...\n\n"
-  
-  function createDocs() {
+
+  function generateDocs(){
+    local srcDir="${1}"
+    local dllName="${2}"
+    local namespace="${3}"
+    local framework="${4}"
+    local gitSrcRootLink="${5}"
+
+    local tempDir="${BUILD_DOCS}/xml-doc-temp"
+    local sourcePath="${tempDir}/${dllName}"
+    local outputPath="${BUILD_DOCS}/md"
+
     cd "${PROJECT_ROOT}"
     dotnet tool restore
-    local sourcePath="${BUILD_UNPACKAGED_DIST}/LanguageExt.Extras.dll"
-    local outputPath="${BUILD_DOCS}/md"
+    mkdir -p "${tempDir}"
+
+    dotnet publish "${srcDir}" \
+      --configuration Release \
+      --output "${tempDir}" \
+      --framework "${framework}" \
+      -p:Version="${PROJECT_VERSION_DIST}" \
+      -p:GenerateDocumentationFile=true
+      
     dotnet xmldocmd "${sourcePath}" "${outputPath}" \
-      --namespace "Jds.LanguageExt.Extras" \
-      --source "https://github.com/JeremiahSanders/languageext-extras/tree/main/src" \
+      --namespace "${namespace}" \
+      --source "${gitSrcRootLink}" \
       --newline lf \
-      --visibility public
+      --visibility protected
+
+    rm -rf "${tempDir}"
+  }
+  
+  function createDocs() {
+    # Use of .net6 needed due to xmldocmd only supporting through net6
+    generateDocs "src" "LanguageExt.Extras.dll" "Jds.LanguageExt.Extras" "net6.0" "https://github.com/JeremiahSanders/languageext-extras/tree/main/src"
   }
 
   ci-dotnet-publish \
+    --framework "net8.0" \
     -p:GenerateDocumentationFile=true &&
     ci-dotnet-pack \
       -p:GenerateDocumentationFile=true &&
